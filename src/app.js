542ed1a7ab20c7b8f82412fdd834f3f98be7a541
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2024-11-26 22:56:17 +0530

    react

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
-
 // import { HydratedRouter, RouterProvider } from "react-router-dom";
 const AppLayout = () => {
   return (
