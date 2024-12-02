9ac70e2285c56c50af1d15ffd85fbe81cbf5e54c
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2024-12-02 22:53:35 +0530

    react

--- src/app.js
+++ src/app.js
@@ -1,29 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Link, Route, Router } from "wouter"; // Importing Wouter
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
-import { createBrowserRouter } from "react-router-dom";
 
-const AppLayout = () => {
+const Home = () => (
+  <div>
+    <Header />
+    <Body />
+  </div>
+);
+function App() {
   return (
-    <div className="app">
-      <Header />
-      <Body />
-    </div>
+    <Router>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+      </nav>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </Router>
   );
-};
-
-// const BrowserRouter = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <AppLayout />,
-//   },
-//   {
-//     path: "/About",
-//     element: <About />,
-//   }
-// ])
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<App />);
